8cbae0dd050ca7ef45ebe5ef5f2f07a8f4fbbc91
laplamgor <laplamgor@gmail.com>
2021-05-15 19:38:14 +0800

    Refactor code

--- js/main.js
+++ js/main.js
@@ -2,208 +2,27 @@
 {
     'use strict';
 
-
-var frag = 
-`precision mediump float;
-uniform vec2 offset;
-uniform vec2 pan;
-uniform float zoom;
-uniform int displayMode;
-
-uniform sampler2D uSampler;
-uniform sampler2D displacementMap;
-
-uniform float textureScale;
-uniform vec2 textureSize;
-uniform float textureWidth;
-uniform float textureHeight;
-uniform float frameWidth;
-uniform float frameHeight;
-uniform vec2 canvasSize;
-
-uniform vec4 filterArea;
-uniform vec4 filterClamp;
-
-
-varying vec2 vTextureCoord;
-varying vec4 vColor;
-uniform vec4 dimensions;
-uniform vec2 mapDimensions;
-uniform float scale;
-uniform float focus;
-
-
-vec2 mapPan(vec2 coord)
-{
-    return vec2((coord[0]  + pan[0] / textureWidth / textureScale) / zoom,
-                (coord[1] + pan[1] / textureHeight / textureScale) / zoom);
-}
-
-vec2 mapCoord2(vec2 coord)
-{
-    return vec2(coord[0] * frameWidth / textureWidth / textureScale,
-                coord[1] * frameHeight / textureHeight / textureScale);
-}
-
-
-vec4 textureDiffuseNoBg(vec2 coord)
-{
-    vec2 c = coord;
-    vec2 scale = textureSize * ( min(canvasSize[0] / textureSize[0], canvasSize[1] / textureSize[1]) );
-
-    c -= 0.5;                   // Normalize
-    c = c * canvasSize + pan;   // Convert to pixel count, where origin is the center
-    c /= scale;
-
-    c /= zoom;
-    c += 0.5;                   // Unnormalize
-
-
-    if (c[0] <= 0.0 || c[0] >= 1.0 || c[1] <= 0.0 || c[1] >= 1.0)
-    {
-        return vec4(0.0);
-    }
-    else
-    {
-        return texture2D(uSampler, c);
-    }
-}
-
-
-vec4 textureDiffuse(vec2 coord)
-{
-    vec4 withoutBg = textureDiffuseNoBg(coord);
-
-    return withoutBg * withoutBg[3] + vec4(0.5, 0.5, 1.0, 1.0) * (1.0 - withoutBg[3]);
-}
-
-vec4 textureDepth(vec2 coord)
-{
-    vec2 c = coord;
-
-    vec2 scale = textureSize * ( min(canvasSize[0] / textureSize[0], canvasSize[1] / textureSize[1]) );
-
-    c -= 0.5;                   // Normalize
-    c = c * canvasSize + pan;   // Convert to pixel count, where origin is the center
-    c /= scale;
-
-    c /= zoom;
-    c += 0.5;                   // Unnormalize
-
-
-    // if (c[0] <= 0.0 || c[0] >= 1.0 || c[1] <= 0.0 || c[1] >= 1.0)
-    // {
-    //     return vec4(0.0, 0.0, 0.0, 0.0);
-    // }
-    return texture2D(displacementMap, c);
-}
-
-vec4 normal(vec2 coord)
-{
-    vec2 lineW = vec2(0.5 / frameWidth, 0.0);
-    vec2 lineH = vec2(0.0, 0.5 / frameHeight);
-
-
-    float leftD = textureDepth(coord - lineW).r;
-    float rightD = textureDepth(coord + lineW).r;
-    float upD = textureDepth(coord - lineH).r;
-    float downD = textureDepth(coord + lineH).r;
-
-    if (textureDiffuseNoBg(coord)[3] < 1.0)
-    {
-        return vec4(0.5, 0.5, 1.0, 1.0);
-    }
-
-    return vec4(0.5, 0.5, 1.0, 1.0) + vec4(leftD - rightD, upD - downD, 0.0, 0.0) * 100.0 * zoom;
-}
-
-vec4 normalMixed(vec2 coord)
-{
-    return textureDiffuse(coord)  - vec4(0.5, 0.5, 1.0, 1.0) + normal(coord);
-}
-
-const float compression = 1.0;
-const float dmin = 0.0;
-const float dmax = 1.0;
-
-// sqrt(2)
-#define MAXOFFSETLENGTH 1.41421356
-// 10 * 1.1
-#define MAXZOOM 11.0
-
-#define MAXSTEPS 600.0
-
-
-float fit = min(canvasSize[0] / textureSize[0], canvasSize[1] / textureSize[1]);
-float steps = max(MAXSTEPS *length(offset *zoom *fit), 30.0);
-
-void main(void)
-{
-    vec2 scale2 = scale * vec2(textureHeight / frameWidth,
-                               textureWidth / frameHeight )
-                  * vec2(1, -1);
-    mat2 baseVector =
-        mat2(vec2((0.5 - focus) * (offset * zoom * fit) - (offset * zoom * fit) / 2.0) * scale2,
-             vec2((0.5 - focus) * (offset * zoom * fit) + (offset * zoom * fit) / 2.0) * scale2);
-
-
-    vec2 pos = (vTextureCoord);
-    mat2 vector = baseVector;
-
-    float dstep = compression / (steps - 1.0);
-    vec2 vstep = (vector[1] - vector[0]) / vec2((steps - 1.0));
-
-    vec2 posSumLast = vec2(0.0);
-    vec2 posSum = vec2(0.0);
-
-    float weigth = 1.0;
-    float dpos;
-    float dposLast;
-
-    for (float i = 0.0; i < MAXSTEPS * MAXOFFSETLENGTH * MAXZOOM; ++i)
-    {
-        vec2 vpos = pos + vector[1] - i * vstep;
-        dpos = 1.0 - i * dstep;
-        float depth = textureDepth(vpos).r;
-
-        if (textureDiffuseNoBg(vpos)[3] == 0.0)
-        {
-            depth = 0.0;
-        }
-
-        depth = clamp(depth, dmin, dmax);
-
-        if (dpos > depth)
-        {
-            posSumLast = vpos;
-            dposLast = dpos;
-        }
-        else
-        {
-            posSum = vpos;
-            weigth = (depth - dposLast) / dstep;
-            break;
-        }
-    };
-    vec2 coord = (posSum - posSumLast) * -clamp(weigth * 0.5 + 0.5, 0.0, 1.5) + posSum;
-    if (displayMode == 0)
-    {
-        gl_FragColor = textureDiffuse(coord);
-    }
-    else if (displayMode == 1)
-    {
-        gl_FragColor = normal(coord);
-    }
-    else
-    {
-        gl_FragColor = normalMixed(coord);
-    }
-
-}`;
-
-
     function init()
     {
+
+        var imageData2;
+
+        var curOnTexX = 0;
+        var curOnTexY = 0;
+
+        // Get texture data as buffer
+        var path = './0463_7319_grmdtyheocuc_depth.png';
+        var img2 = new Image();
+        img2.onload = function ()
+        {
+            var tempCanvas = document.createElement("CANVAS");;
+            tempCanvas.width = img2.width;
+            tempCanvas.height = img2.height;
+            const ctx = tempCanvas.getContext('2d');
+            ctx.drawImage(img2, 0, 0);
+            imageData2 = ctx.getImageData(0, 0, tempCanvas.width, tempCanvas.height);
+        }
+        img2.src = path;
 
         PIXI.DepthPerspectiveFilter = new PIXI.Filter(null, frag);
 
@@ -235,15 +54,18 @@ void main(void)
             var fit = Math.min(window.displacementFilter.uniforms.canvasSize[0] / window.displacementFilter.uniforms.textureSize[0], 
                 window.displacementFilter.uniforms.canvasSize[1] / window.displacementFilter.uniforms.textureSize[1]);
 
-            bunny.x = (app.renderer.plugins.interaction.mouse.global.x - window.displacementFilter.uniforms.canvasSize[0] / 2 + window.displacementFilter.uniforms.textureSize[0] / 2 * zoom * fit);
-            bunny.x += window.displacementFilter.uniforms.pan[0];
-            bunny.x = bunny.x / zoom / fit;
-            bunny.x = bunny.x / window.displacementFilter.uniforms.textureSize[0] * window.displacementFilter.uniforms.canvasSize[0]  ;
+            curOnTexX = (app.renderer.plugins.interaction.mouse.global.x - window.displacementFilter.uniforms.canvasSize[0] / 2 + window.displacementFilter.uniforms.textureSize[0] / 2 * zoom * fit);
+            curOnTexX += window.displacementFilter.uniforms.pan[0];
+            curOnTexX = curOnTexX / zoom / fit;
+            bunny.x = curOnTexX / window.displacementFilter.uniforms.textureSize[0] * window.displacementFilter.uniforms.canvasSize[0]  ;
 
-            bunny.y = (app.renderer.plugins.interaction.mouse.global.y - window.displacementFilter.uniforms.canvasSize[1] / 2 + window.displacementFilter.uniforms.textureSize[1] / 2 * zoom * fit);
-            bunny.y += window.displacementFilter.uniforms.pan[1];
-            bunny.y = bunny.y / zoom / fit;
-            bunny.y = bunny.y / window.displacementFilter.uniforms.textureSize[1] * window.displacementFilter.uniforms.canvasSize[1] ;
+            curOnTexY = (app.renderer.plugins.interaction.mouse.global.y - window.displacementFilter.uniforms.canvasSize[1] / 2 + window.displacementFilter.uniforms.textureSize[1] / 2 * zoom * fit);
+            curOnTexY += window.displacementFilter.uniforms.pan[1];
+            curOnTexY = curOnTexY / zoom / fit;
+            bunny.y = curOnTexY / window.displacementFilter.uniforms.textureSize[1] * window.displacementFilter.uniforms.canvasSize[1] ;
+
+            bunnyReverse.x = bunny.x;
+            bunnyReverse.y = bunny.y;
 
         }
 
@@ -269,13 +91,15 @@ void main(void)
         window.displacementFilter.uniforms.displayMode = 2;
 
         var container = new PIXI.Container();
-        var bunny = new PIXI.Sprite(PIXI.Texture.from('https://pixijs.io/examples/examples/assets/bunny.png'));
+        var bunny = new PIXI.Sprite(PIXI.Texture.WHITE);
+        var bunnyReverse = new PIXI.Sprite(PIXI.Texture.WHITE);
         bunny.anchor.set(0.5);
+        bunnyReverse.anchor.set(0.5);
         container.filters = [window.displacementFilter];
         app.stage.addChild(container);
-        container.addChild(bunny);
         container.addChild(depthMapImage);
         container.addChild(bunny);
+        container.addChild(bunnyReverse);
 
         var tiltX;
         var tiltY;
@@ -308,13 +132,20 @@ void main(void)
                 alert('You have a strange Mouse!');
             }
 
-            var zoom = window.displacementFilter.uniforms.zoom;
-            console.log('pan ' + window.displacementFilter.uniforms.pan[0] + '   ' + window.displacementFilter.uniforms.pan[1]);
-            console.log('zoom ' + zoom);
+            // var zoom = window.displacementFilter.uniforms.zoom;
+            // console.log('pan ' + window.displacementFilter.uniforms.pan[0] + '   ' + window.displacementFilter.uniforms.pan[1]);
+            // console.log('zoom ' + zoom);
 
-            var fit = Math.min(window.displacementFilter.uniforms.canvasSize[0] / window.displacementFilter.uniforms.textureSize[0], 
-                window.displacementFilter.uniforms.canvasSize[1] / window.displacementFilter.uniforms.textureSize[1]);
-            console.log('fit ' + fit);
+            // var fit = Math.min(window.displacementFilter.uniforms.canvasSize[0] / window.displacementFilter.uniforms.textureSize[0], 
+            //     window.displacementFilter.uniforms.canvasSize[1] / window.displacementFilter.uniforms.textureSize[1]);
+            // console.log('fit ' + fit);
+
+
+            if (imageData2 && imageData2.data) {
+                console.log('curOnTexX: ' + curOnTexX + '  curOnTexY: ' + curOnTexY);
+
+                console.log(imageData2.data[(Math.round(curOnTexY) * window.displacementFilter.uniforms.textureSize[0] + Math.round(curOnTexX) )* 4 ]);
+            }
 
         }
         );
@@ -569,6 +400,209 @@ void main(void)
             return newPos;
         }
     }
+
+
+
+
+var frag = 
+`precision mediump float;
+uniform vec2 offset;
+uniform vec2 pan;
+uniform float zoom;
+uniform int displayMode;
+
+uniform sampler2D uSampler;
+uniform sampler2D displacementMap;
+
+uniform float textureScale;
+uniform vec2 textureSize;
+uniform float textureWidth;
+uniform float textureHeight;
+uniform float frameWidth;
+uniform float frameHeight;
+uniform vec2 canvasSize;
+
+uniform vec4 filterArea;
+uniform vec4 filterClamp;
+
+
+varying vec2 vTextureCoord;
+varying vec4 vColor;
+uniform vec4 dimensions;
+uniform vec2 mapDimensions;
+uniform float scale;
+uniform float focus;
+
+
+vec2 mapPan(vec2 coord)
+{
+    return vec2((coord[0]  + pan[0] / textureWidth / textureScale) / zoom,
+                (coord[1] + pan[1] / textureHeight / textureScale) / zoom);
+}
+
+vec2 mapCoord2(vec2 coord)
+{
+    return vec2(coord[0] * frameWidth / textureWidth / textureScale,
+                coord[1] * frameHeight / textureHeight / textureScale);
+}
+
+
+vec4 textureDiffuseNoBg(vec2 coord)
+{
+    vec2 c = coord;
+    vec2 scale = textureSize * ( min(canvasSize[0] / textureSize[0], canvasSize[1] / textureSize[1]) );
+
+    c -= 0.5;                   // Normalize
+    c = c * canvasSize + pan;   // Convert to pixel count, where origin is the center
+    c /= scale;
+
+    c /= zoom;
+    c += 0.5;                   // Unnormalize
+
+
+    if (c[0] <= 0.0 || c[0] >= 1.0 || c[1] <= 0.0 || c[1] >= 1.0)
+    {
+        return vec4(0.0);
+    }
+    else
+    {
+        return texture2D(uSampler, c);
+    }
+}
+
+
+vec4 textureDiffuse(vec2 coord)
+{
+    vec4 withoutBg = textureDiffuseNoBg(coord);
+
+    return withoutBg * withoutBg[3] + vec4(0.5, 0.5, 1.0, 1.0) * (1.0 - withoutBg[3]);
+}
+
+vec4 textureDepth(vec2 coord)
+{
+    vec2 c = coord;
+
+    vec2 scale = textureSize * ( min(canvasSize[0] / textureSize[0], canvasSize[1] / textureSize[1]) );
+
+    c -= 0.5;                   // Normalize
+    c = c * canvasSize + pan;   // Convert to pixel count, where origin is the center
+    c /= scale;
+
+    c /= zoom;
+    c += 0.5;                   // Unnormalize
+
+
+    // if (c[0] <= 0.0 || c[0] >= 1.0 || c[1] <= 0.0 || c[1] >= 1.0)
+    // {
+    //     return vec4(0.0, 0.0, 0.0, 0.0);
+    // }
+    return texture2D(displacementMap, c);
+}
+
+vec4 normal(vec2 coord)
+{
+    vec2 lineW = vec2(0.5 / frameWidth, 0.0);
+    vec2 lineH = vec2(0.0, 0.5 / frameHeight);
+
+
+    float leftD = textureDepth(coord - lineW).r;
+    float rightD = textureDepth(coord + lineW).r;
+    float upD = textureDepth(coord - lineH).r;
+    float downD = textureDepth(coord + lineH).r;
+
+    if (textureDiffuseNoBg(coord)[3] < 1.0)
+    {
+        return vec4(0.5, 0.5, 1.0, 1.0);
+    }
+
+    return vec4(0.5, 0.5, 1.0, 1.0) + vec4(leftD - rightD, upD - downD, 0.0, 0.0) * 100.0 * zoom;
+}
+
+vec4 normalMixed(vec2 coord)
+{
+    return textureDiffuse(coord)  - vec4(0.5, 0.5, 1.0, 1.0) + normal(coord);
+}
+
+const float compression = 1.0;
+const float dmin = 0.0;
+const float dmax = 1.0;
+
+// sqrt(2)
+#define MAXOFFSETLENGTH 1.41421356
+// 10 * 1.1
+#define MAXZOOM 11.0
+
+#define MAXSTEPS 600.0
+
+
+float fit = min(canvasSize[0] / textureSize[0], canvasSize[1] / textureSize[1]);
+float steps = max(MAXSTEPS *length(offset *zoom *fit), 30.0);
+
+void main(void)
+{
+    vec2 scale2 = scale * vec2(textureHeight / frameWidth,
+                               textureWidth / frameHeight )
+                  * vec2(1, -1);
+    mat2 baseVector =
+        mat2(vec2((0.5 - focus) * (offset * zoom * fit) - (offset * zoom * fit) / 2.0) * scale2,
+             vec2((0.5 - focus) * (offset * zoom * fit) + (offset * zoom * fit) / 2.0) * scale2);
+
+
+    vec2 pos = (vTextureCoord);
+    mat2 vector = baseVector;
+
+    float dstep = compression / (steps - 1.0);
+    vec2 vstep = (vector[1] - vector[0]) / vec2((steps - 1.0));
+
+    vec2 posSumLast = vec2(0.0);
+    vec2 posSum = vec2(0.0);
+
+    float weigth = 1.0;
+    float dpos;
+    float dposLast;
+
+    for (float i = 0.0; i < MAXSTEPS * MAXOFFSETLENGTH * MAXZOOM; ++i)
+    {
+        vec2 vpos = pos + vector[1] - i * vstep;
+        dpos = 1.0 - i * dstep;
+        float depth = textureDepth(vpos).r;
+
+        if (textureDiffuseNoBg(vpos)[3] == 0.0)
+        {
+            depth = 0.0;
+        }
+
+        depth = clamp(depth, dmin, dmax);
+
+        if (dpos > depth)
+        {
+            posSumLast = vpos;
+            dposLast = dpos;
+        }
+        else
+        {
+            posSum = vpos;
+            weigth = (depth - dposLast) / dstep;
+            break;
+        }
+    };
+    vec2 coord = (posSum - posSumLast) * -clamp(weigth * 0.5 + 0.5, 0.0, 1.5) + posSum;
+    if (displayMode == 0)
+    {
+        gl_FragColor = textureDiffuse(coord);
+    }
+    else if (displayMode == 1)
+    {
+        gl_FragColor = normal(coord);
+    }
+    else
+    {
+        gl_FragColor = normalMixed(coord);
+    }
+
+}`;
+
+
     init();
 }
     ());
